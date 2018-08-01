37e4d017deac810d51a0ac1e71bc4838d0c09c64
Lenovo <Lenovo@LAPTOP-3ABJAA55>
2018-08-01 09:18:37 +0700

    Initial Commit

---
+++ src/jobsheeet7/Jobsheeet7.java
@@ -0,0 +1,12 @@
+
+package jobsheeet7;
+
+
+public class Jobsheeet7 {
+    public static void main(String[] args) {
+        String identitas = "nama lengkap / kelas / no";
+        System.out.println("Identitas : "+ identitas );
+    
+    }
+    
+}
